89f4c809d70c29beb02fef067db50be1a0809ded
MirandaCreative <admin@mirandacreative.com>
2018-02-07 11:06:00 -0500

    Updated wordpress and fixed accordion module

--- wp-content/themes/MC-genesis-boilerplate/parts/flexible/accordions.php
+++ wp-content/themes/MC-genesis-boilerplate/parts/flexible/accordions.php
@@ -15,9 +15,11 @@
 	    	$atwotitle = get_sub_field('accordion_two_title');	    	
 	    	$aonecontent = get_sub_field('accordion_one_content');	 
 	    	$atwocontent = get_sub_field('accordion_two_content');	
+	    	$fullwidth = get_sub_field('full_width');
 
-		if ($amtofcols >= 1){
-	      echo '<div class="col-sm-12">
+		if ($amtofcols == 1){
+			if($fullwidth = 1 ){$colwidth = '12';}else{$colwidth = '6';}
+	      echo '<div class="col-sm-'.$colwidth.'">
 		    			<div id="accordionfull-'.$uid.'" role="tablist" aria-multiselectable="true">
 							<div class="card">
 								<div class="card-header" role="tab" id="headingOne">
@@ -35,9 +37,30 @@
 								</div>
 							</div>
 						</div>
-				</div>';
-				echo '<div class="w-100"></div>';
-				if ($amtofcols == 2){				
+				</div>';				
+			};
+
+if ($amtofcols == 2){
+	      echo '<div class="col-sm-6">
+		    			<div id="accordionhalfy-'.$uid.'" role="tablist" aria-multiselectable="true">
+							<div class="card">
+								<div class="card-header" role="tab" id="headingOne">
+								      <h5 class="mb-0">
+								        <a data-toggle="collapse" data-parent="#accordionhalfy-'.$uid.'" href="#collapsehalfy-'.$uid.'" aria-expanded="true" aria-controls="collapsehalfy-'.$uid.'">
+								          '.$aonetitle.'
+								        </a>
+								      </h5>
+								    </div>
+
+								    <div id="collapsehalfy-'.$uid.'" class="collapse show" role="tabpanel" aria-labelledby="headingOne-'.$uid.'">
+								      <div class="card-block">
+								      <h1>'.$fullwidth.'</h1>
+								      '.$aonecontent.'
+								      </div>
+								</div>
+							</div>
+						</div>
+				</div>';								
 				      echo '<div class="col-sm-6">
 					    			<div id="accordiontwo-'.$uid.'" role="tablist" aria-multiselectable="true">
 										<div class="card">
@@ -59,8 +82,6 @@
 							</div>';
 					echo '<div class="w-100"></div>';	
 			}
-			};
-
 	
 
 
