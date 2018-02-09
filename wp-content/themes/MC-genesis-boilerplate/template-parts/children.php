a2ce81f79ee953f1761a63109eb15d5c8e6792d3
MirandaCreative <admin@mirandacreative.com>
2018-02-09 16:43:31 -0500

    made-links-children-page-dynamic

--- wp-content/themes/MC-genesis-boilerplate/template-parts/children.php
+++ wp-content/themes/MC-genesis-boilerplate/template-parts/children.php
@@ -3,16 +3,16 @@
       <div class="col-sm-3">
          <div class="block">
             <div class="rel">
-               <img src="http://www.wethersfieldlibrary.dev.cc/wp-content/themes/MC-genesis-boilerplate/images/grownwl.svg">
+               <img src="<?php echo get_home_url(); ?>/wp-content/themes/MC-genesis-boilerplate/images/grownwl.svg">
                <h2 class="abs grnups">Grown Ups</h2>
             </div>
          </div>
          <div class="block">
-            <div class="rel"><img class="margblock" src="http://www.wethersfieldlibrary.dev.cc/wp-content/themes/MC-genesis-boilerplate/images/left-books.png"></div>
+            <div class="rel"><img class="margblock" src="<?php echo get_home_url(); ?>/wp-content/themes/MC-genesis-boilerplate/images/left-books.png"></div>
          </div>
          <div class="block">
             <div class="rel">
-               <img class="easle" src="http://www.wethersfieldlibrary.dev.cc/wp-content/themes/MC-genesis-boilerplate/images/programswl.svg">
+               <img class="easle" src="<?php echo get_home_url(); ?>/wp-content/themes/MC-genesis-boilerplate/images/programswl.svg">
                <h2 class="abs prgms">Programs</h2>
             </div>
          </div>
@@ -20,46 +20,46 @@
       <div class="col-sm-6">
          <div class="block">
             <div class="rel">
-            	<img src="http://www.wethersfieldlibrary.dev.cc/wp-content/themes/MC-genesis-boilerplate/images/todays-message-bg.png">
+            	<img src="<?php echo get_home_url(); ?>/wp-content/themes/MC-genesis-boilerplate/images/todays-message-bg.png">
 	            <p class="abs todaymsg">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dignissim leo ut nibh placerat, scelerisque convallis mi consequat. Sed maximus semper ex quis vehicula. Pellentesque a massa vitae ipsum sagittis suscipit.
             	</p>
             </div>
          </div>
          <div class="block">
             <div class="rel">
-            	<img class="rug" src="http://www.wethersfieldlibrary.dev.cc/wp-content/themes/MC-genesis-boilerplate/images/carpetwl.svg">
+            	<img class="rug" src="<?php echo get_home_url(); ?>/wp-content/themes/MC-genesis-boilerplate/images/carpetwl.svg">
             	<p class="abs kidstablet">App/Website of the month</p>
-            	<img class="abs girlone" src="http://www.wethersfieldlibrary.dev.cc/wp-content/themes/MC-genesis-boilerplate/images/girl1wl.svg"> 
-            	<img class="abs tabletgirl" src="http://www.wethersfieldlibrary.dev.cc/wp-content/themes/MC-genesis-boilerplate/images/girltabletwl.svg">
-            	<img class="abs tabletboy" src="http://www.wethersfieldlibrary.dev.cc/wp-content/themes/MC-genesis-boilerplate/images/boywl.svg">            	            	           	
+            	<img class="abs girlone" src="<?php echo get_home_url(); ?>/wp-content/themes/MC-genesis-boilerplate/images/girl1wl.svg"> 
+            	<img class="abs tabletgirl" src="<?php echo get_home_url(); ?>/wp-content/themes/MC-genesis-boilerplate/images/girltabletwl.svg">
+            	<img class="abs tabletboy" src="<?php echo get_home_url(); ?>/wp-content/themes/MC-genesis-boilerplate/images/boywl.svg">            	            	           	
             </div>
          </div>
       </div>
       <div class="col-sm-3">
          <div class="block">
             <div class="rel">
-           		<img class="games" src="http://www.wethersfieldlibrary.dev.cc/wp-content/themes/MC-genesis-boilerplate/images/gameswl.svg">  
+           		<img class="games" src="<?php echo get_home_url(); ?>/wp-content/themes/MC-genesis-boilerplate/images/gameswl.svg">  
             </div>
          </div>
          <div class="block">
             <div class="rel">
-           		<img class="thoubooks" src="http://www.wethersfieldlibrary.dev.cc/wp-content/themes/MC-genesis-boilerplate/images/bookswl.svg">  
+           		<img class="thoubooks" src="<?php echo get_home_url(); ?>/wp-content/themes/MC-genesis-boilerplate/images/bookswl.svg">  
             </div>
          </div>
          <div class="block">  
             <div class="rel">
-           		<img class="teddyshelf" src="http://www.wethersfieldlibrary.dev.cc/wp-content/themes/MC-genesis-boilerplate/images/shelfwl.svg">  
-           		<img class="abs teddy" src="http://www.wethersfieldlibrary.dev.cc/wp-content/themes/MC-genesis-boilerplate/images/bearwl.svg">           		
+           		<img class="teddyshelf" src="<?php echo get_home_url(); ?>/wp-content/themes/MC-genesis-boilerplate/images/shelfwl.svg">  
+           		<img class="abs teddy" src="<?php echo get_home_url(); ?>/wp-content/themes/MC-genesis-boilerplate/images/bearwl.svg">           		
             </div>
          </div>
          <div class="block">
             <div class="rel">
-           		<img class="greatread" src="http://www.wethersfieldlibrary.dev.cc/wp-content/themes/MC-genesis-boilerplate/images/readswl.svg">  
+           		<img class="greatread" src="<?php echo get_home_url(); ?>/wp-content/themes/MC-genesis-boilerplate/images/readswl.svg">  
             </div>
          </div>
          <div class="block">
             <div class="rel">
-           		<img class="abs toys" src="http://www.wethersfieldlibrary.dev.cc/wp-content/themes/MC-genesis-boilerplate/images/toyswl.svg">  
+           		<img class="abs toys" src="<?php echo get_home_url(); ?>/wp-content/themes/MC-genesis-boilerplate/images/toyswl.svg">  
             </div>
          </div>
       </div>
