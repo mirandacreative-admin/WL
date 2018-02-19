01287882e5f345dcde15f1dfcc324a341d8cb277
MikaCaldera <mcaldera@mirandacreative.com>
2018-02-19 15:48:56 -0500

    Changed the header, main menu navigation, hover states, borders, Removed a lot of css, Added padding to content-sidebar-wrap but I don't think it's a good idea.

--- wp-content/themes/MC-genesis-boilerplate/template-parts/slidemenu-top.php
+++ wp-content/themes/MC-genesis-boilerplate/template-parts/slidemenu-top.php
@@ -1,26 +1,24 @@
-  <div class="wsmenucontainer clearfix">
-    <div id="overlapblackbg"></div>
-    <div class="header">
-      <div class="wrapper clearfix"> <a id="wsnavtoggle" class="animated-arrow"><span></span></a> 
-      </div>
-
-      <!--Main Menu HTML Code-->
-      <nav class="wsmenu clearfix">
-        <ul class="mobile-sub wsmenu-list clearfix">
-          <li>
-            <img class="sublogo tkmuseo" src="<?php echo get_home_url(); ?>/wp-content/uploads/2018/01/logo.png"> 
-            <span class="subtag">Wethersfield Library</span>
-          </li>
-          <?php wp_nav_menu(array(
-               'menu' => 'main-menu', 
-               'items_wrap'=>'%3$s', 
-               'container' => false
-            )); ?>   
-        <?php get_template_part('template-parts/sticky-sidebar-menu');?>                   
-        </ul>
-      </nav>          
- 
-      <!--Menu HTML Code-->
-
-    </div>
-    <div class="wrapper"> <br>        
+<div class="wsmenucontainer clearfix">
+  <div id="overlapblackbg"></div>
+  <div class="header">
+    <div class="wrapper clearfix"> <a id="wsnavtoggle" class="animated-arrow"><span></span></a>
+  </div>
+  <!--Main Menu HTML Code-->
+  <nav class="wsmenu clearfix">
+    <ul class="mobile-sub wsmenu-list clearfix">
+      <li>
+        <img class="sublogo tk-museo" src="<?php echo get_home_url(); ?>/wp-content/uploads/2018/01/logo.png">
+        <span class="subtag">Wethersfield Library</span>
+      </li>
+      <?php wp_nav_menu(array(
+      'menu' => 'main-menu',
+      'items_wrap'=>'%3$s',
+      'container' => false
+      )); ?>
+      <?php get_template_part('template-parts/sticky-sidebar-menu');?>
+    </ul>
+  </nav>
+  
+  <!--Menu HTML Code-->
+</div>
+<div class="wrapper"> <br>
--- wp-content/themes/MC-genesis-boilerplate/template-parts/sticky-sidebar-menu.php
+++ wp-content/themes/MC-genesis-boilerplate/template-parts/sticky-sidebar-menu.php
@@ -6,23 +6,23 @@
 			<ul class="dow">
 				<li>Mon</li>
 				<li>Tues</li>
-				<li>Wed</li>	
+				<li>Wed</li>
 				<li>Thurs</li>
 				<li>Fri</li>
-				<li>Sat</li>		
-				<li>Sun</li>					
+				<li>Sat</li>
+				<li>Sun</li>
 			</ul>
 		</div>
 		<div class="col-8">
-		<ul class="hours">
-			<li name="Mon"><?php the_field('monday_hours', 'option'); ?></li>
-			<li name="Tue"><?php the_field('tuesday_hours', 'option'); ?></li>
-			<li name="Wed"><?php the_field('wednesday_hours', 'option'); ?></li>	
-			<li name="Thu"><?php the_field('thursday_hours', 'option'); ?></li>
-			<li name="Fri"><?php the_field('friday_hours', 'option'); ?></li>
-			<li name="Sat"><?php the_field('saturday_hours', 'option'); ?></li>		
-			<li name="Sun"><?php the_field('sunday_hours', 'option'); ?></li>		
-		</ul>			
+			<ul class="hours">
+				<li name="Mon"><?php the_field('monday_hours', 'option'); ?></li>
+				<li name="Tue"><?php the_field('tuesday_hours', 'option'); ?></li>
+				<li name="Wed"><?php the_field('wednesday_hours', 'option'); ?></li>
+				<li name="Thu"><?php the_field('thursday_hours', 'option'); ?></li>
+				<li name="Fri"><?php the_field('friday_hours', 'option'); ?></li>
+				<li name="Sat"><?php the_field('saturday_hours', 'option'); ?></li>
+				<li name="Sun"><?php the_field('sunday_hours', 'option'); ?></li>
+			</ul>
 		</div>
 	</div>
 	<h2>Connect</h2>
@@ -30,16 +30,16 @@
 		<li class="contactus"><a href="#">Contact Us</a></li>
 		<li class="enews"><a href="#">Sign Up For ENews</a></li>
 		<li class="directions"><a href="#">Directions</a></li>
-		<li class="fb"><a href="#">Facebook</a></li>				
+		<li class="fb"><a href="#">Facebook</a></li>
 		<li class="tw"><a href="#">Twitter</a></li>
-	</ul>	
+	</ul>
 	<div class="librarysupportbox">
 		<span>SUPPORT<br/>THE LIBRARY</span>
 	</div>
 	<p class="address">
-		515 Silas Deane Highway  Wethersfield, CT 06109 
+		515 Silas Deane Highway  Wethersfield, CT 06109
 	</p>
 	<p class="phonenumber">
 		(860) 529-2665
 	</p>
-	</div>
+</div>
