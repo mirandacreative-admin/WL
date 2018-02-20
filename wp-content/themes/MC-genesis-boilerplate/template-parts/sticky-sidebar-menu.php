a6e11e45bf4c5e1efea1e32f9e24805dcb0aa256
MikaCaldera <mcaldera@mirandacreative.com>
2018-02-20 11:01:13 -0500

    Sidebar Finalized
    Finished the sidebar  - the Icons need to be switched to SVGs no PNGs.
    Made edits to html & Git ignore.

--- wp-content/themes/MC-genesis-boilerplate/template-parts/sticky-sidebar-menu.php
+++ wp-content/themes/MC-genesis-boilerplate/template-parts/sticky-sidebar-menu.php
@@ -1,7 +1,6 @@
 <div id="left-sticky">
 	<img class="logo" src="<?php the_field('logo', 'option'); ?>">
 	<a href="#"><h2>About Library</h2></a>
-
 	<div class="hourblock">
 		<h2>Library Hours</h2>
 		<div class="row hour-inner">
@@ -29,22 +28,22 @@
 			</div>
 		</div>
 	</div>
-
 	<h2>Connect</h2>
 	<ul class="quicklinks">
-		<li class="contactus"><a href="#">Contact Us</a></li>
+		<li class="friends"><a href="#">Friends of the Library</a></li>
+		<li class="contact"><a href="#">Contact Us</a></li>
 		<li class="enews"><a href="#">Sign Up For ENews</a></li>
 		<li class="directions"><a href="#">Directions</a></li>
-		<li class="fb"><a href="#">Facebook</a></li>
-		<li class="tw"><a href="#">Twitter</a></li>
+		<li class="facebook"><a href="#">Facebook</a></li>
+		<li class="twitter"><a href="#">Twitter</a></li>
 	</ul>
-	<div class="librarysupportbox">
-		<span>SUPPORT<br/>THE LIBRARY</span>
-	</div>
-	<p class="address">
+	<button type="button" class="button">
+		SUPPORT<br/>THE LIBRARY
+	</button>
+	<address>
+		<span class="address">
 		515 Silas Deane Highway  Wethersfield, CT 06109
-	</p>
-	<p class="phonenumber">
-		(860) 529-2665
-	</p>
+		</span>
+		<a href="+18605292665">(860) 529-2665</a>
+	</address>
 </div>
