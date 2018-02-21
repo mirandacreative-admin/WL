1d3ae9ab4e8cc30e4e74e73a74c88387f522cbee
MikaCaldera <mcaldera@mirandacreative.com>
2018-02-21 15:39:19 -0500

    Working On Grid Cards
    Working on the structure for grid cards - I may have broken it? It needs reviewings.

--- wp-content/themes/MC-genesis-boilerplate/parts/flexible/card-grid.php
+++ wp-content/themes/MC-genesis-boilerplate/parts/flexible/card-grid.php
@@ -1,35 +1,27 @@
-<?php 
-
-        	// check if the nested repeater field has rows of data
-        	if( have_rows('cards') ):
-				$count = 0; 
-			 	echo '<div class="row squaregrid padfix">';
-
-			 	// loop through the rows of data
-			    while ( have_rows('cards') ) : the_row();
-			    $count++;
+<?php
+/**
+* Card Content Grid
+*/
+?>
+<?php if (have_rows('cards')): ?>
+<section class="row squaregrid padfix">
+	<?php while(have_rows('cards')): the_row();
 		$title = get_sub_field('title');
 		$blurb = get_sub_field('blurb');
-		$link_text = get_sub_field('link_text');	
+		$link_text = get_sub_field('link_text');  
 		$ext = get_sub_field('ext');
-		$link_url = get_sub_field('link_url');	
-		$post_link = get_sub_field('post_link');	
+		$link_url = get_sub_field('link_url');  
+		$post_link = get_sub_field('post_link');  
 		$hover_color = get_sub_field('hover_color');
-		$target = '_self';
-		if ($ext = 'yes'){
-			$target = '_blank';
-		}			
+	?>
 
-					echo '<div onmouseover="this.style.background=\''.$hover_color.'\'" onmouseout="this.style.background=\'white\'" class="col">
-					<h2><a target="'.$target.'" href="'.$link_url.'">'.$link_text.'</a></h2>
-					<p>'.$blurb.'</p>
-					</div>';
-					// are the column counts divisible by three so we can end the row
-			if ( $count % 3 == 0 ){ echo '<div class="w-100"></div>';}
+<div onmouseover="this.style.background=\''.<?php echo $hover_color; ?>.'\'" onmouseout="this.style.background=\'white\'" class="col">
+	<a href="<?php echo $link_url; ?>">
+	<h2><?php echo $title; ?></h2>
+	<p><?php echo $blurb; ?></p>
+	</a>
+</div>
 
-
-				endwhile;
-
-				echo '</div>';
-
-			endif; ?>
+	<?php endwhile; ?>
+</section>
+<?php endif; ?>
