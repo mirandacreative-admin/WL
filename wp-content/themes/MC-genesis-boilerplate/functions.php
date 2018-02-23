a431eb533fd9d9fcb26ae3f670d30d8b35aa89bd
MikaCaldera <mcaldera@mirandacreative.com>
2018-02-23 14:25:03 -0500

    Card Grid
    Finished up the CSS for card gird.  The php document still needs work.

--- wp-content/themes/MC-genesis-boilerplate/functions.php
+++ wp-content/themes/MC-genesis-boilerplate/functions.php
@@ -189,9 +189,9 @@ function mc_enqueue_my_styles() {
     
     wp_enqueue_style( 'font-awesome', get_stylesheet_directory_uri() . '/font-awesome/css/font-awesome.min.css' );   
       
-    wp_enqueue_style( 'ws-style', get_stylesheet_directory_uri() . '/assets/css/webslidemenu.css' );    
-    wp_enqueue_style( 'custom-css', get_stylesheet_directory_uri() . '/custom.css' );      
-     
+    wp_enqueue_style( 'ws-style', get_stylesheet_directory_uri() . '/assets/css/webslidemenu.css' );
+
+    wp_enqueue_style( 'custom-css', get_stylesheet_directory_uri() . '/custom.css' );     
 }
 // webslide menu code
 
--- wp-content/themes/MC-genesis-boilerplate/parts/flexible/card-grid.php
+++ wp-content/themes/MC-genesis-boilerplate/parts/flexible/card-grid.php
@@ -3,24 +3,24 @@
 * Card Content Grid
 */
 ?>
+
 <?php if (have_rows('cards')): ?>
 <section class="row squaregrid padfix">
 	<?php while(have_rows('cards')): the_row();
 		$title = get_sub_field('title');
 		$blurb = get_sub_field('blurb');
-		$link_text = get_sub_field('link_text');  
-		$ext = get_sub_field('ext');
+		//$link_text = get_sub_field('link_text');  
+		//$ext = get_sub_field('ext');
 		$link_url = get_sub_field('link_url');  
-		$post_link = get_sub_field('post_link');  
+		//$post_link = get_sub_field('post_link');  
 		$hover_color = get_sub_field('hover_color');
 	?>
-
-<div onmouseover="this.style.background=\''.<?php echo $hover_color; ?>.'\'" onmouseout="this.style.background=\'white\'" class="col">
-	<a href="<?php echo $link_url; ?>">
-	<h2><?php echo $title; ?></h2>
-	<p><?php echo $blurb; ?></p>
+	<a class="col-link" href="<?php echo $link_url; ?>">
+		<div onmouseover="this.style.background=\''.<?php echo $hover_color; ?>.'\'" onmouseout="this.style.background=\'white\'" class="col">
+			<h2><?php echo $title; ?></h2>
+			<p><?php echo $blurb; ?></p>
+		</div>
 	</a>
-</div>
 
 	<?php endwhile; ?>
 </section>
