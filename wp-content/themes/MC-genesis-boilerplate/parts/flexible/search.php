ecdcfbace4d76de60943c8d5949cefcadcb34ada
MikaCaldera <mcaldera@mirandacreative.com>
2018-03-06 16:23:45 -0500

    Adding Google Materials & Search edits

--- wp-content/themes/MC-genesis-boilerplate/parts/flexible/search.php
+++ wp-content/themes/MC-genesis-boilerplate/parts/flexible/search.php
@@ -6,8 +6,9 @@ Search Bars
 <section class="searchbars">
 
   <form method="get" action="http://lci-mt.iii.com/iii/encore/Home,$Search.form.sdirect" name="form" id="catform">
-    <input class="" name="target" placeholder="Search Library Catalog.." id="" size="" type="text">
+    <input class="" name="target" placeholder="Search Library Catalog" id="" size="" type="text">
     <input type="submit" value="" class="submit">
+    <i class="material-icons">accessible</i>
   </form>
 
   <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Search Widget") ) : ?>
