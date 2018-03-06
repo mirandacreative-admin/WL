7bbf143b8f9c393e7342ea3b2a409e2df4bf33c9
MikaCaldera <mcaldera@mirandacreative.com>
2018-03-06 17:06:29 -0500

    quick fix to card and search

--- wp-content/themes/MC-genesis-boilerplate/parts/flexible/search.php
+++ wp-content/themes/MC-genesis-boilerplate/parts/flexible/search.php
@@ -8,11 +8,11 @@ Search Bars
   <form method="get" action="http://lci-mt.iii.com/iii/encore/Home,$Search.form.sdirect" name="form" id="catform">
     <input class="" name="target" placeholder="Search Library Catalog" id="" size="" type="text">
     <input type="submit" value="" class="submit">
-    <i class="material-icons">accessible</i>
+    <i class="material-icons">search</i>
   </form>
 
-  <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Search Widget") ) : ?>
-  <?php endif;?>
+  <?php //if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Search Widget") ) : ?>
+  <?php //endif;?>
   
   <?php get_search_form(); ?>
 
