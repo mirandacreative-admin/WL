2687ea1d3f46327d84f14c18ee21e042b76ad3b3
MirandaCreative <admin@mirandacreative.com>
2018-03-07 14:11:41 -0500

    added custom search

--- wp-content/themes/MC-genesis-boilerplate/parts/flexible/search.php
+++ wp-content/themes/MC-genesis-boilerplate/parts/flexible/search.php
@@ -4,17 +4,25 @@ Search Bars
 */
 ?>
 <section class="searchbars">
-
+<div class="searchwrapper">
   <form method="get" action="http://lci-mt.iii.com/iii/encore/Home,$Search.form.sdirect" name="form" id="catform">
-    <input class="" name="target" placeholder="Search Library Catalog" id="" size="" type="text">
-    <input type="submit" value="" class="submit">
-    <i class="material-icons">search</i>
+    <input class="" name="target"  onkeydown="alert('shweet')" placeholder="Search Library Catalog" id="libcat" size="" type="text">
+    <input type="image" value="" class="submit imgsubmit" src="<?php echo get_home_url(); ?>/wp-content/themes/MC-genesis-boilerplate/images/search-icon.png">
   </form>
-
+</div>
   <?php //if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Search Widget") ) : ?>
   <?php //endif;?>
-  
-  <?php get_search_form(); ?>
-
+<div class="searchwrapper">  
+	<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
+	    <label style="width: 100%;">
+	        <span class="screen-reader-text"><?php echo _x( 'Search for:', 'label' ) ?></span>
+	        <input id="wpsearch"  onkeydown="alert('shweet')" type="search" class="search-field"
+	            placeholder="<?php echo esc_attr_x( 'Search …', 'placeholder' ) ?>"
+	            value="<?php echo get_search_query() ?>" name="s"
+	            title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
+	    </label>
+	        <input type="image" class="imgsubmit" alt="Search" src="<?php echo get_home_url(); ?>/wp-content/themes/MC-genesis-boilerplate/images/search-icon.png" />
+	</form>
+</div>
 
 </section>
