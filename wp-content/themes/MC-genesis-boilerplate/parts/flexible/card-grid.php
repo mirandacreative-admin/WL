42dc0a9a02c83564ebd09091285ae3bbc22dd128
MirandaCreative <admin@mirandacreative.com>
2018-03-17 11:36:20 -0400

    finally fixed the external link switcher

--- wp-content/themes/MC-genesis-boilerplate/parts/flexible/card-grid.php
+++ wp-content/themes/MC-genesis-boilerplate/parts/flexible/card-grid.php
@@ -21,7 +21,7 @@ Card Content Grid
                 $hover_color = get_sub_field('hover_color');
             ?>
             <div class="hold col-lg-4 col-sm-6">
-            <a class="link" onmouseover="this.style.background='<?php echo $hover_color; ?>'"onmouseout="this.style.background='white'" href="<?php echo $link_url; ?>">
+            <a class="link" onmouseover="this.style.background='<?php echo $hover_color; ?>'"onmouseout="this.style.background='white'" href="<?php echo $linkurl; ?>">
                 <div>
                     <h2 class="header"><?php echo $title; ?></h2>
                     <div class="blurb"><?php echo $blurb; ?></div>
