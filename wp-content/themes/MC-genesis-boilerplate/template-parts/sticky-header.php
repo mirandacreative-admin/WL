2216dfc011479dcc8c03bba5a65a6cb45abffbab
Workstation30 <TK@Workstation30s-iMac.local>
2018-03-27 16:16:50 -0400

    fixing about-portal link below the logo

--- wp-content/themes/MC-genesis-boilerplate/template-parts/sticky-header.php
+++ wp-content/themes/MC-genesis-boilerplate/template-parts/sticky-header.php
@@ -6,27 +6,31 @@
 
 <div id="stickem" class="float-header container">
 
-	<div class="row">
-		<div class="col-md-2">
-			<a href="<?php echo get_option("siteurl"); ?>"><img class="homelink" style="width: 100%; height: 100%;" src="<?php echo get_home_url(); ?>/wp-content/themes/MC-genesis-boilerplate/images/spacer.png"></a>
-		</div>
-		<div class="col-md-10 navpanel">
-			<div class="wrapper">
-				<div class="logogroup">
-					<a href="<?php echo get_option("siteurl"); ?>"><img class="mediumlogo" src="<?php the_field('logo', 'option'); ?>"></a>
-					<h1 class="tk-museo"><?php bloginfo( 'name' ); ?></h1>
-				</div>
-				<!-- Main Menu-->
-				<?php 
-					wp_nav_menu(
-						array(
-							'menu' => 'main-menu',
-							'container_class' => 'main-menu',
-							'menu_class' => 'nav',
-						)
-					);
+    <div class="row">
+        <div class="col-md-2">
+            <a href="<?php echo get_option( "siteurl" ); ?>">
+                <img class="homelink"
+                     src="<?php echo get_home_url(); ?>/wp-content/themes/MC-genesis-boilerplate/images/spacer.png">
+            </a>
+        </div>
+        <div class="col-md-10 navpanel">
+            <div class="wrapper">
+                <div class="logogroup">
+                    <a href="<?php echo get_option( "siteurl" ); ?>"><img class="mediumlogo"
+                                                                          src="<?php the_field( 'logo', 'option' ); ?>"></a>
+                    <h1 class="tk-museo"><?php bloginfo( 'name' ); ?></h1>
+                </div>
+                <!-- Main Menu-->
+				<?php
+				wp_nav_menu(
+					array(
+						'menu'            => 'main-menu',
+						'container_class' => 'main-menu',
+						'menu_class'      => 'nav',
+					)
+				);
 				?>
-			</div>
-		</div>
-	</div>
+            </div>
+        </div>
+    </div>
 </div>
