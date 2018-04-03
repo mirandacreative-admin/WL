9754f80250d8c12c6c3722cbcca886acb07bbec3
MirandaCreative <admin@mirandacreative.com>
2018-04-03 16:36:05 -0400

    added pdf uploadability to sidebar color buttons

--- wp-content/themes/MC-genesis-boilerplate/template-parts/sticky-sidebar.php
+++ wp-content/themes/MC-genesis-boilerplate/template-parts/sticky-sidebar.php
@@ -7,7 +7,24 @@ Right Sidebar
 
 <?php if (have_rows('color_links', 'options')): ?>
   <?php while (have_rows('color_links', 'options')): the_row(); ?>
-    <a class="button" style="background-color: <?php the_sub_field('button_color'); ?>;" href="<?php the_sub_field('button_link'); ?>" target="_blank">
+  	<?php $isitpdf = get_sub_field('isitpdf');
+  	      $pdffile = get_sub_field('pdf_upload'); 
+  		  $pagelink = get_sub_field('button_link');
+  		  
+  		  // if($pdffile)
+  		  // { 
+  		  // 	$finalink = $pdffile;
+  		  // 	$target = 'download'; 
+  		  // } else { 
+  		  // 	$finalink = $pagelink; 
+  		  // 	$target = ''; 
+  		  // }
+
+  		  $finalink = ($pdffile) ? $pdffile : $pagelink;
+  		  $target = ($pdffile) ? 'download' : '';  
+
+  	?>
+    <a class="button" style="background-color: <?php the_sub_field('button_color'); ?>;" href="<?php echo $finalink; ?>" target="_blank" <?= $target; ?> >
     <?php the_sub_field('button_text'); ?>
     </a>
   <?php endwhile; ?>
